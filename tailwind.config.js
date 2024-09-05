d11112d7ed2801dd85883013c50782bf7579eeb3
alistairjoelquinn <alistairjoelquinn@gmail.com>
2024-09-05 23:15:02 +0200

    adds more homepage layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.{html,js}'],
+  content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playpen: ['"Playpen Sans"', "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
