3c9598ddea51e5c84c9ab6045c4bbe70b379beb2
alistairjoelquinn <alistairjoelquinn@gmail.com>
2024-09-08 23:07:32 +0200

    Removes debug logs

--- src/aws/s3.rs
+++ src/aws/s3.rs
@@ -1,9 +1,12 @@
 use std::sync::Arc;
 
+use crate::state::State;
 use anyhow::{Context, Result};
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{config::Region, Client};
-use axum::{response::Html, Extension};
+use axum::http::{header, StatusCode};
+use axum::response::{IntoResponse, Response};
+use axum::Extension;
 use dotenv::dotenv;
 
 #[derive(Debug)]
@@ -25,11 +28,7 @@ impl S3Client {
                 .load()
                 .await;
 
-        println!("Shared config: {:#?}", shared_config);
-
         let client = Client::new(&shared_config);
-
-        println!("Client: {:?}", client);
 
         Ok(Self { client }) // Your other methods...
     }
@@ -47,24 +46,39 @@ pub async fn init_s3() -> Result<Arc<S3Client>> {
         }
     }
 }
-
 pub async fn get_s3_object(
     Extension(state): Extension<Arc<State>>,
-) -> Result<Html<&'static str>, anyhow::Error> {
-    println!("running Koen code");
-    let obj = state
-        .s3
-        .client
-        .get_object()
-        .bucket("your-bucket-name")
-        .key("path/to/image.jpg")
-        .send()
-        .await
-        .context("Failed to get object from S3")?;
-    let _data = obj
-        .body
-        .collect()
-        .await
-        .context("Failed to collect S3 object body")?;
-    Ok(Html("<p>hi</p>"))
+) -> impl IntoResponse {
+    async {
+        println!("Fetching image from S3");
+        let obj = state
+            .s3
+            .client
+            .get_object()
+            .bucket("your-bucket-name")
+            .key("path/to/image.jpg")
+            .send()
+            .await
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+        let data = obj
+            .body
+            .collect()
+            .await
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
+            .into_bytes();
+
+        let content_type = obj
+            .content_type
+            .unwrap_or("application/octet-stream".to_string());
+
+        Ok::<_, (StatusCode, String)>(
+            Response::builder()
+                .status(StatusCode::OK)
+                .header(header::CONTENT_TYPE, content_type)
+                .body(axum::body::Body::from(data))
+                .unwrap(),
+        )
+    }
+    .await
 }
