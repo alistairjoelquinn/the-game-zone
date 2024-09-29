3d188bc7debc3c75c6350a67064d40e4e62f062a
alistairjoelquinn <alistairjoelquinn@gmail.com>
2024-09-29 13:22:20 +0200

    removes duplicate fade in class

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,13 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
       },
       keyframes: {
-        fadein: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
-      },
-      keyframes: {
         fadeIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
       },
       animation: {
-        "fade-in": "fadeIn 0.5s ease-in-out",
-      },
-      animation: {
-        fadein: "fadein 1s forwards",
+        "fade-in": "fadeIn 0.5s forwards",
       },
     },
     plugins: [],
