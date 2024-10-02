c5c720b186d8a18047401539c1379832e1c6906f
alistairjoelquinn <alistairjoelquinn@gmail.com>
2024-10-02 20:47:09 +0200

    refactors utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,30 +1,7 @@
 pub mod auth;
 
-use axum::http::{
-    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
-    HeaderValue, Method, StatusCode,
-};
+use axum::http::StatusCode;
 use axum::BoxError;
-use chrono::{DateTime, Local};
-use tower_http::cors::CorsLayer;
-
-pub fn get_time() -> DateTime<Local> {
-    let now = Local::now();
-    now
-}
-
-pub fn initialise_cors() -> CorsLayer {
-    CorsLayer::new()
-        .allow_origin("http://localhost:3333".parse::<HeaderValue>().unwrap())
-        .allow_methods([
-            Method::POST,
-            Method::GET,
-            Method::PATCH,
-            Method::DELETE,
-        ])
-        .allow_credentials(true)
-        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
-}
 
 pub async fn handle_timeout_error(err: BoxError) -> (StatusCode, String) {
     if err.is::<tower::timeout::error::Elapsed>() {
