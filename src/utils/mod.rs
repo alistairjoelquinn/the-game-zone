64d403ad543a71f4e5c9f90d285b74138c7d5a4e
alistairjoelquinn <alistairjoelquinn@gmail.com>
2024-10-03 16:44:50 +0200

    refactors cors config

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -10,12 +10,7 @@ use tower_http::cors::CorsLayer;
 pub fn initialise_cors() -> CorsLayer {
     CorsLayer::new()
         .allow_origin("http://localhost:3333".parse::<HeaderValue>().unwrap())
-        .allow_methods([
-            Method::POST,
-            Method::GET,
-            Method::PATCH,
-            Method::DELETE,
-        ])
+        .allow_methods([Method::POST, Method::GET])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
 }
