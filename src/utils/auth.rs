1c75ff9cf741236a970954de23a5bf468a995f79
alistairjoelquinn <alistairjoelquinn@gmail.com>
2024-10-04 18:39:18 +0200

    adds functions for handling pashword hasing

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use axum::http::StatusCode;
+use bcrypt::{hash, verify, DEFAULT_COST};
 use chrono::{Duration, Utc};
 use jsonwebtoken::{encode, EncodingKey, Header};
 use serde::{Deserialize, Serialize};
@@ -26,4 +28,12 @@ pub fn encode_jwt(
         &EncodingKey::from_secret(secret.as_ref()),
     )
     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+pub async fn hash_password(password: &str) -> Result<String> {
+    Ok(hash(password, DEFAULT_COST)?)
+}
+
+pub async fn verify_password(password: &str, hash: &str) -> Result<bool> {
+    Ok(verify(password, hash)?)
 }
